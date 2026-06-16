0139de60291c621c74f9082a36ac870b944cd720
Jiajun Hou <hjj15937985362@hotmail.com>
2026-06-16 08:30:53 +0800

    fix: load service_registry first to define ai.base.service before dependent services

--- custom_addons/ai_core/services/__init__.py
+++ custom_addons/ai_core/services/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from . import service_registry       # MUST be first: defines ai.base.service
 from . import document_parser
 from . import chunker
 from . import embedding_service
 from . import vector_store
 from . import rag_service
 from . import ai_chat_service
-from . import service_registry
